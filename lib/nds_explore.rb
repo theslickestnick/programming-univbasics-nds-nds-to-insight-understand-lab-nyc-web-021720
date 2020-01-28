1800d756f6a878b11b2ef7385691f1d7c54d2912
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-28 18:34:22 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,9 +10,10 @@ end
 pretty_print_nds(directors_database)
 
 
-# def print_first_directors_movie_titles
-#     row_index = 0
-#   while row_index < directors_database["Stephen Spielberg"][:movies].length do
-#     puts :title.to_s
-#   end
-# end
+def print_first_directors_movie_titles
+    row_index = 0
+    while row_index < directors_database[0][:movies].length do
+      puts directors_database[0][:movies][row_index][:title]
+      row_index += 1
+    end
+end
